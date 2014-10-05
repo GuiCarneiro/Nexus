6a144fc3695cea32ef6ad3dbd790144c2c998063
Gui Carneiro <gui.carneiro15@gmail.com>
2014-10-05 00:13:49 -0300

    Static Requests - Complete

--- lib/nexus/static.rb
+++ lib/nexus/static.rb
@@ -9,12 +9,12 @@ module Nexus
 	def self.get_champion_by_name(champ_name,champData = "all")
 		url = ("#{@global_uri}#{@region}/#{VERSION_STATIC}/champion?champData=#{champData}&api_key=#{@api_key}")
 		response = retrieve_url get(url)
-		return (response["data"][champ_name.to_s])
+		return Nexus::Champion.new(response["data"][champ_name.to_s])
 	end
 
 	def self.get_champion_by_id(champ_id, champData = "all")
 		url = ("#{@global_uri}#{@region}/#{VERSION_STATIC}/champion/#{champ_id}?champData=#{champData}&api_key=#{@api_key}")
-		return (retrieve_url get(url))
+		return Nexus::Champion.new(retrieve_url get(url))
 	end
 
 	def self.get_items(itemData = "all")
@@ -38,17 +38,6 @@ module Nexus
 	def self.get_mastery_by_id(mastery_id, masteryData = "all")
 		url = ("#{@global_uri}#{@region}/#{VERSION_STATIC}/mastery/#{mastery_id}?masteryData=#{masteryData}&api_key=#{@api_key}")
 		return Nexus::Mastery.new(retrieve_url get(url))
-	end
-
-	def self.get_runes(runeData = "all")
-		url = ("#{@global_uri}#{@region}/#{VERSION_STATIC}/rune?runeListData=#{runeData}&api_key=#{@api_key}")
-		response = retrieve_url get(url)
-		return (response["data"])
-	end
-
-	def self.get_rune_by_id(rune_id, runeData = "all")
-		url = ("#{@global_uri}#{@region}/#{VERSION_STATIC}/rune/#{rune_id}?runeData=#{runeData}&api_key=#{@api_key}")
-		return (retrieve_url get(url))
 	end
 
 	def self.get_runes(runeData = "all")
