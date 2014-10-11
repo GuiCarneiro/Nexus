3fcc1f0934ac9d11001c3754276ff89a2025104c
Gui Carneiro <gui.carneiro15@gmail.com>
2014-10-10 22:24:12 -0300

    Game Controller - Done

--- lib/nexus.rb
+++ lib/nexus.rb
@@ -7,6 +7,7 @@ require_relative 'nexus/connection'
 require_relative 'nexus/controller/summoner'
 require_relative 'nexus/controller/static'
 require_relative 'nexus/controller/status'
+require_relative 'nexus/controller/game'
 
 #Models#
 require_relative 'nexus/model/summoner'
@@ -14,4 +15,5 @@ require_relative 'nexus/model/champion'
 require_relative 'nexus/model/masteries'
 require_relative 'nexus/model/items'
 require_relative 'nexus/model/runes'
+require_relative 'nexus/model/game'
 
